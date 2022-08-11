3f5fb3cc39893f46bb1fe46bcef955eb80972761
川端うらら <>
2022-08-11 18:56:29 +0900

    コントローラー記述したけどコメントアウトしておいたやつ

--- AtlasSNS/app/Http/Controllers/UsersController.php
+++ AtlasSNS/app/Http/Controllers/UsersController.php
@@ -35,4 +35,31 @@ class UsersController extends Controller
 
         return view('users.search', compact('users', 'keyword'));
     }
+
+    // 8/11記述
+        // フォロー
+    // public function follow(User $user)
+    // {
+    //     $follower = auth()->user();
+    //     // フォローしているか
+    //     $is_following = $follower->isFollowing($user->id);
+    //     if(!$is_following) {
+    //         // フォローしていなければフォローする
+    //         $follower->follow($user->id);
+    //         return back();
+    //     }
+    // }
+
+    // // フォロー解除
+    // public function unfollow(User $user)
+    // {
+    //     $follower = auth()->user();
+    //     // フォローしているか
+    //     $is_following = $follower->isFollowing($user->id);
+    //     if($is_following) {
+    //         // フォローしていればフォローを解除する
+    //         $follower->unfollow($user->id);
+    //         return back();
+    //     }
+    // }
 }
